ed4a125e4ccb7be0492abb2de392dd8bb1471402
Babeena <babeenak020@gmail.com>
2023-09-06 17:58:38 +0530

    overload operator assignment

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            employee example = new employee();
+
+            example.firstName = "Kitty";
+            example.lastName = "Katz";
+            example.ID = 24923;
+
+            employee example2 = new employee();
+            example2.firstName = "John";
+            example2.lastName = "Dudinsky";
+            example2.ID = 39292;
+
+            employee.Equals(example2, example);
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   public class employee
+    {
+        public int ID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public static bool Equals(employee emp1, employee emp2)
+        {
+            if (emp1.ID == emp2.ID)
+                return true;
+            else
+                return false;
+        }
+    }
+    }
+}
